d844388b8f87250f3219113e402ef269a8ccd61a
Jason Schuller <hi@jason.sc>
2014-03-15 21:39:48 -0700

    Removing "get_template_part" on the 404 page.

--- content-404.php
+++ content-404.php
@@ -1,8 +1,4 @@
 <div id="background" class="row" <?php if (is_image('background')) { ?>style="background-image: url('<?php get_setting($site_url); ?>/uploads/background.jpg');"<?php } ?>>
-    <div class="row">
-        <?php get_template_part('content-header'); ?>
-    </div>
-    
     <div id="page" class="row">
         <div class="content">
             <h2><?php get_text($not_found_title); ?></h2>
